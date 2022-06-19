992c0699be63d29700a77475f87d1ef793a777fa
John Marquess <john@marquess.me>
2022-06-19 14:29:29 +1000

    Bat products

--- left_joins.R
+++ left_joins.R
@@ -1,2 +1,14 @@
 source("load_join_data.R")
 
+inventory_parts_joined <- inventories %>% 
+  inner_join(inventory_parts, by = c("id" = "inventory_id")) %>% 
+  select(-id, -version) %>% 
+  arrange(desc(quantity))
+
+batmobile <- inventory_parts_joined %>% 
+  filter(set_num == "7784-1") %>% 
+  select(-set_num)
+
+batwing <- inventory_parts_joined %>% 
+  filter(set_num == "70916-1") %>% 
+  select(-set_num)
