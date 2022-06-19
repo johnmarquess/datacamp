9cd9df2222a3d5166cbabcb8a76cf7c079d66261
John Marquess <john@marquess.me>
2022-06-20 07:30:25 +1000

    Full joins

--- full_joins.R
+++ full_joins.R
@@ -1,26 +1,17 @@
 source("load_join_data.R")
 
-inventory_parts_joined <- inventories %>%
-  inner_join(inventory_parts, by = c("id" = "inventory_id")) %>%
-  arrange(desc(quantity)) %>%
-  select(-id, -version)
+batman_parts <- batman %>%
+  count(part_num, color_id, wt = quantity)
 
-inventory_parts_joined
+star_wars_parts <- star_wars %>%
+  count(part_num, color_id, wt = quantity)
 
-inventory_sets_themes <- inventory_parts_joined %>% 
-  inner_join(sets,  by = "set_num") %>% 
-  inner_join(themes, by = c("theme_id" =  "id"), suffix = c("_set", "_theme"))
-
-inventory_sets_themes
-
-batman <- inventory_sets_themes %>%
-  filter(name_theme == "Batman")
-
-star_wars <- inventory_sets_themes %>%
-  filter(name_theme == "Star Wars")
+batman_parts
+star_wars_parts
 
 
-batman %>% count(part_num, color_id, wt = quantity)
-star_wars %>% count(part_num, color_id, wt = quantity)
 
-
+batman_parts %>% 
+  full_join(star_wars_parts, by = c("part_num", "color_id"), suffix = c("_batman", "_star_wars")) %>% 
+  replace_na(list(n_batman = 0,
+                  n_star_wars = 0))
