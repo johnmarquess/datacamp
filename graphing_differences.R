fa4e7ef18f6fdc3db37dc327879f06a1f714a0df
John Marquess <john@marquess.me>
2022-06-20 18:36:48 +1000

    Graph - lego

--- graphing_differences.R
+++ graphing_differences.R
@@ -1,15 +1,41 @@
 source("load_join_data.R")
 
+
+inventory_parts_themes <- inventories %>%
+  inner_join(inventory_parts, by = c("id" = "inventory_id")) %>%
+  arrange(desc(quantity)) %>%
+  select(-id, -version) %>%
+  inner_join(sets, by = "set_num") %>%
+  inner_join(themes, by = c("theme_id" = "id"), suffix = c("_set", "_theme"))
+
+
+inventory_parts_themes %>% count(name_theme, sort = TRUE) 
+
 batman_colors <- inventory_parts_themes %>%
-  # Filter the inventory_parts_themes table for the Batman theme
-  filter(name_theme == ___) %>%
+  filter(name_theme == "Batman") %>%
   group_by(color_id) %>%
   summarize(total = sum(quantity)) %>%
-  # Add a fraction column of the total divided by the sum of the total 
-  mutate(___)
+  mutate(fraction = total / sum(total))
 
-# Filter and aggregate the Star Wars set data; add a fraction column
 star_wars_colors <- inventory_parts_themes %>%
-  ___
+  filter(name_theme == "Star Wars") %>%
+  group_by(color_id) %>%
+  summarize(total = sum(quantity)) %>%
+  mutate(fraction = total / sum(total))
 
+colors_joined <- batman_colors %>%
+  full_join(star_wars_colors, by = "color_id", suffix = c("_batman", "_star_wars")) %>%
+  replace_na(list(total_batman = 0, total_star_wars = 0)) %>%
+  inner_join(colors, by = c("color_id" = "id")) %>%
+  mutate(difference = fraction_batman - fraction_star_wars,
+         total = total_batman + total_star_wars) %>%
+  filter(total >= 200) %>%
+  mutate(name = fct_reorder(name, difference)) 
+  
 
+# Create a bar plot using colors_joined and the name and difference columns
+ggplot(colors_joined, aes(x= difference, y = name, fill = color_id)) +
+  geom_col() +
+  coord_flip() +
+  scale_fill_manual(values = color_palette, guide = "none") +
+  labs(y = "Difference: Batman - Star Wars")
